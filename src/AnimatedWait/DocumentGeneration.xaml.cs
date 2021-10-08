3355d09e68dc40998bdcbbf9fbb3310e8242cb46
Jeremy Demortier <jeremy.demortier@nbb.be>
2021-10-08 11:49:32 +0200

    - Add DocumentGeneration - Add publish workflow

---
+++ src/AnimatedWait/DocumentGeneration.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AnimatedWait
+{
+    public partial class DocumentGeneration : UserControl
+    {
+        public DocumentGeneration()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty FileBackgroundBrushProperty = DependencyProperty.Register(
+            "FileBackgroundBrush" , typeof( Brush ) , typeof( DocumentGeneration ) , new PropertyMetadata( Brushes.GhostWhite ) );
+
+        public Brush FileBackgroundBrush
+        {
+            get => (Brush) GetValue( FileBackgroundBrushProperty );
+            set => SetValue( FileBackgroundBrushProperty , value );
+        }
+
+        public static readonly DependencyProperty FileBorderBrushProperty = DependencyProperty.Register(
+            "FileBorderBrush" , typeof( Brush ) , typeof( DocumentGeneration ) , new PropertyMetadata( Brushes.DimGray ) );
+
+        public Brush FileBorderBrush
+        {
+            get => (Brush) GetValue( FileBorderBrushProperty );
+            set => SetValue( FileBorderBrushProperty , value );
+        }
+
+        public static readonly DependencyProperty FileContentBrushProperty = DependencyProperty.Register(
+            "FileContentBrush" , typeof( Brush ) , typeof( DocumentGeneration ) , new PropertyMetadata( Brushes.DarkGray ) );
+
+        public Brush FileContentBrush
+        {
+            get => (Brush) GetValue( FileContentBrushProperty );
+            set => SetValue( FileContentBrushProperty , value );
+        }
+
+        public static readonly DependencyProperty RepeatBehaviorProperty = DependencyProperty.Register(
+            "RepeatBehavior" , typeof( RepeatBehavior ) , typeof( DocumentGeneration ) , new PropertyMetadata( RepeatBehavior.Forever ) );
+
+        public RepeatBehavior RepeatBehavior
+        {
+            get => (RepeatBehavior) GetValue( RepeatBehaviorProperty );
+            set => SetValue( RepeatBehaviorProperty , value );
+        }
+    }
+}
